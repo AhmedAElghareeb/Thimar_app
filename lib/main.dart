313dc9e0b6a2aa9e5328e2931d3ef7c479c86714
Ahmed Elghareeb <ahmed.elghareeb1166@gmail.com>
2024-05-28 14:14:20 +0300

    notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thimar_app/core/logic/cache_helper.dart';
+import 'package:thimar_app/core/logic/firebase_notifications.dart';
 import 'package:thimar_app/core/logic/helper_methods.dart';
 import 'package:thimar_app/generated/codegen_loader.g.dart';
 import 'package:thimar_app/views/auth/splash.dart';
 import 'core/logic/kiwi.dart';
 import 'core/logic/main_data.dart';
-import 'firebase_options.dart';
 
 GetIt getIt = GetIt.instance;
+
+late SharedPreferences Prefs;
 void initServiceLocator() {
   getIt.registerLazySingleton<AppGlobals>(() => AppGlobals());
    }
@@ -22,19 +24,13 @@ Future<void> main() async {
   initKiwi();
   initServiceLocator();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseMessaging.instance.getToken().then(
-    (value) {
-      print("FCM Token is : $value");
-    },
-  );
+  Prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
+  await GlobalNotification().setUpFirebase();
   CacheHelper.init();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: getMaterialColor(),
-      statusBarIconBrightness: Brightness.light,
     ),
   );
   runApp(
@@ -61,6 +57,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         navigatorKey: navigatorKey,
         theme: ThemeData(
+          useMaterial3: false,
           appBarTheme: AppBarTheme(
             backgroundColor: const Color(
               0xffFFFFFF,
